51ea6d7ebe6576cf57c8dc222c20f904a9c74266
ibrahimFOH <ibrahimkavasoglu7@gmail.com>
2026-08-04 01:46:41 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 // =======================
-// FOTOĞRAFLAR (MANUEL AMA ESNEK)
+// FOTOĞRAFLAR
 // =======================
 
 const photos = [
-"resimler/galeri/kskzpxoxkdkodld.jpg",
-"resimler/galeri/123454849939229.jpg",
-"resimler/galeri/asdasdadasd.jpeg"
+"images/gallery/0EAAE007-14C6-468D-80CB-6C5275CB6827.jpeg",
+"images/gallery/1028BA7C-0A7F-49DF-B2DE-896109D700EC.jpeg",
+"images/gallery/104D23F8-EA6F-40B7-866B-CF689E065E46.jpeg",
+"images/gallery/11AD6679-02D7-4ADC-87AA-92059E510189.jpeg",
+"images/gallery/1BAAC2CA-6D55-4FB6-8459-5E8EAD6D9C8E.jpeg",
+"images/gallery/35E63E2A-949E-4AD5-AE11-E607E868C697.jpeg",
+"images/gallery/3EB8D62A-EB98-4976-950D-E74C17848A2C.jpeg",
+"images/gallery/4AFED6AF-BB13-46FD-8613-6F556058AFE7.jpeg",
+"images/gallery/668E9605-0CDE-435F-A8DF-8B5888BB6C32.jpeg",
+"images/gallery/BFAD4788-CF18-4F44-BAA3-43060965EEEA.jpeg",
+"images/gallery/DE228FEA-36C1-4CCD-9185-0E03014CD491.jpeg",
+"images/gallery/E232024B-1E1C-46CD-983E-D108BDDFE7F1.jpeg",
+"images/gallery/F29D89E6-01E9-4111-BC47-5E0293BF883F.jpeg"
 ];
 
 const gallery = document.getElementById("gallery");
@@ -31,8 +41,9 @@ gallery.appendChild(img);
 // =======================
 
 const videos = [
-"videolar/video1.mp4",
-"videolar/randomvideo.mp4"
+"videos/video1.mp4",
+"videos/video2.mp4",
+"videos/video3.mp4"
 ];
 
 const videoContainer = document.getElementById("videos");
@@ -47,7 +58,6 @@ player.src = src;
 player.controls = true;
 player.preload = "metadata";
 
-// aynı anda tek video oynasın
 player.addEventListener("play", () => {
 document.querySelectorAll("video").forEach(v => {
 if (v !== player) v.pause();
@@ -61,7 +71,7 @@ videoContainer.appendChild(player);
 }
 
 // =======================
-// HERO SLIDER (FOTO ARRAY'DEN)
+// HERO SLIDER
 // =======================
 
 const hero = document.querySelector(".hero");
@@ -88,7 +98,7 @@ url('${photos[heroIndex]}') center/cover no-repeat
 }
 
 // =======================
-// LIGHTBOX (GALERİ)
+// LIGHTBOX
 // =======================
 
 document.addEventListener("click", (e) => {
@@ -121,7 +131,7 @@ document.body.appendChild(overlay);
 });
 
 // =======================
-// TEKLİF FORM → WHATSAPP
+// FORM → WHATSAPP
 // =======================
 
 const form = document.getElementById("offerForm");
