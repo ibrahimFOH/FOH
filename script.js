16426ea5d8ccd13e6106b1f986d823f765b1c3c2
ibrahimFOH <ibrahimkavasoglu7@gmail.com>
2026-08-04 11:21:54 +0300

    Update script.js

--- script.js
+++ script.js
@@ -162,3 +162,25 @@ window.location.href =
 });
 
 }
+
+// =======================
+// SMOOTH SCROLL
+// =======================
+
+document.querySelectorAll('a[href^="#"]').forEach(link => {
+
+link.addEventListener("click", function(e){
+
+const target = document.querySelector(this.getAttribute("href"));
+
+if(!target) return;
+
+e.preventDefault();
+
+target.scrollIntoView({
+behavior:"smooth"
+});
+
+});
+
+});
